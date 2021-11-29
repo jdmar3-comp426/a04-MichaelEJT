46203a2fe8ee6f3169543be8efbf1799d47691b0
MichaelEJT <Ejt20010427!@>
2021-11-28 23:05:00 -0500

    aaa

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/app/", (req, res, next) => {
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new", (req, res) => {
 	const stmt = db.prepare('INSERT INTO userinfo (user, pass) VALUES (?, ?)').run(req.body.user, md5(req.body.pass));
-	res.status(200).json({"message":"1 record created: ID 3 (200)"});
+	res.status(200).json({"message":"1 record created: ID 3 (201)"});
 });
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
@@ -48,7 +48,7 @@ app.get("/app/user/:id",(req, res) => {
 
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run(req.body.user, md5(req.body.pass), req.params.id);
-	res.status(200).json({"Message":"Successfully updated user (200)"});
+	res.status(200).json({"message":"1 record updated: ID 2 (200)"});
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
