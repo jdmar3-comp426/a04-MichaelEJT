ad736e48da73787f4deffb5223236d4739348955
MichaelEJT <Ejt20010427!@>
2021-11-28 23:07:21 -0500

    updated status

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get("/app/user/:id",(req, res) => {
 
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run(req.body.user, md5(req.body.pass), req.params.id);
-	res.status(200).json({"message":"1 record updated: ID 2 (200)"});
+	res.status(201).json({"message":"1 record updated: ID 2 (200)"});
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
